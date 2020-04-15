3a18c5d8c059c1eaebcc31a8b806863341f8fcce
Gašper Podergajs <gpodergajs@gmail.com>
2020-04-15 18:02:10 +0200

    Add project files.

---
+++ NavtehAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NavtehAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
