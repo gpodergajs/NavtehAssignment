796b6d2f093fe7284e5e00cdb941b6e759f277ee
Gašper Podergajs <gpodergajs@gmail.com>
2020-04-15 19:15:17 +0200

    Add FileUtil
    Added util class for file reading

---
+++ NavtehAssignment/src/_utils/FileUtil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NavtehAssignment.src._utils
+{
+    public static class FileUtil
+    {        
+
+        public static string[] ReadFileLines(string path)
+        {
+            path = "C:\\Users\\gpode\\Desktop\\txt\\test.txt";
+
+            try
+            {
+                if (!File.Exists(path)) throw new FileNotFoundException();
+                return File.ReadAllLines(path);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+        public static string ReadFile(string path)
+        {
+             path = "C:\\Users\\gpode\\Desktop\\txt\\test.txt";
+             try
+             {
+                if (!File.Exists(path)) throw new FileNotFoundException();
+
+                string fileText = File.ReadAllText(path);
+                return fileText;
+
+             }
+             catch (Exception ex)
+             {
+                 throw ex;
+             }
+        }
+    }
+}
