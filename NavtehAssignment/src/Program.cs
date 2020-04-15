29e0018b323f66945b1ca043c549bda4d256d4c5
Gašper Podergajs <gpodergajs@gmail.com>
2020-04-15 19:17:45 +0200

    Updated Program
    Changed location, implemented utils

---
+++ NavtehAssignment/src/Program.cs
@@ -0,0 +1,23 @@
+﻿using NavtehAssignment.src._utils;
+using System;
+using System.Collections.Generic;
+
+namespace NavtehAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] fileTextLines = FileUtil.ReadFileLines("");
+            string fileText = FileUtil.ReadFile("");
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
