df1366701d12ba52ede0f46e9e716e82d8c16712
Gašper Podergajs <gpodergajs@gmail.com>
2020-04-16 19:06:58 +0200

    Create ListUtil.cs
    Added list util class for list ordering and overlap removal

---
+++ NavtehAssignment/src/_utils/ListUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NavtehAssignment.src._utils
+{
+    public static class ListUtil
+    {
+
+        public static List<string> OrderByLength(List<string> stringList)
+        {
+            var sortedStringList = stringList
+            .OrderByDescending(n => n.Length)
+            .ToList();
+
+            return sortedStringList;
+        }
+
+        public static List<string> RemoveOverlappedStrings(List<string> stringList)
+        {
+            List<string> listWithoutOverlap = new List<string>(stringList);
+            foreach (var item in stringList)
+            {
+                var strDupList = stringList.Where(str => item.Contains(str) && !str.Equals(item));
+                listWithoutOverlap = listWithoutOverlap.Except(strDupList).ToList();
+            }
+            return listWithoutOverlap;  
+        }
+    }
+}
