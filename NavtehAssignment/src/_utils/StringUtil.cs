ece58acd0d415bd771608392a03e6048b2b7e657
Gašper Podergajs <gpodergajs@gmail.com>
2020-04-22 18:16:26 +0200

    Create StringUtil.cs
    Added utility class for substring count

---
+++ NavtehAssignment/src/_utils/StringUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavtehAssignment.src._utils
+{
+    public static class StringUtil
+    {
+        public static int CountSubstringOccurence(string source, string substring)
+        {
+            // counts all the occurences
+            int count = 0;
+            int i = 0;
+            while ((i = source.IndexOf(substring, i)) != -1)
+            {
+                i += substring.Length;
+                count++;
+            }
+            return count;
+        }
+    }
+}
